350e30266478cdf37d26bb0dab12339a1f6ac3eb
Bbenekko <bbeneko@outlook.com>
2025-06-10 07:42:36 -0300

    Constantes e variáveis na tabela

--- meuteste.s
+++ meuteste.s
@@ -4,11 +4,45 @@ main:
 
 pushq %rbp
 movq %rsp, %rbp
-subq $32, %rsp
-/*%r11 -> v1 */
-movq %r12, -8(%rbp)    /*r12 -> v2*/
-movq %r13, -16(%rbp)   /*r13 -> v3*/
-movq %r14, -24(%rbp)   /*r14 -> v4*/
-movq %r15, -32(%rbp)   /*r15 -> v5*/
+subq $32, %rsp /*Libera espaço para as 5 variaveis locais*/
 
-/*PARÂMETROS */
+/*Parametros e registradores */
+movl %edi, %r11d        /*p1 -> r11d*/
+movl %esi, %r11d        /*p2 -> r11d*/
+movl %edx, %r11d        /*p3 -> r11d*/
+
+movl %r10d, -4(%rbp)    /*r10d -> v1*/
+movl %r11d, -4(%rbp)    /*r11d -> v1*/
+movl -4(%rbp), %r10d    /*v1 -> %r10d*/
+movl -4(%rbp). %r11d    /*v1 -> %r11d*/
+
+movl %r10d, -8(%rbp)    /*r10d -> v2*/
+movl %r11d, -8(%rbp)    /*r11d -> v2*/
+movl -8(%rbp), %r10d    /*v2 -> %r10d*/
+movl -8(%rbp). %r11d    /*v2 -> %r11d*/
+
+movl %r10d, -12(%rbp)    /*r10d -> v3*/
+movl %r11d, -12(%rbp)    /*r11d -> v3*/
+movl -12(%rbp), %r10d    /*v3 -> %r10d*/
+movl -12(%rbp). %r11d    /*v3 -> %r11d*/
+
+movl %r10d, -16(%rbp)    /*r10d -> v4*/
+movl %r11d, -16(%rbp)    /*r11d -> v4*/
+movl -16(%rbp), %r10d    /*v4 -> %r10d*/
+movl -16(%rbp). %r11d    /*v4 -> %r11d*/
+
+movl %r10d, -20(%rbp)    /*r10d -> v5*/
+movl %r11d, -20(%rbp)    /*r11d -> v5*/
+movl -20(%rbp), %r10d    /*v5 -> %r10d*/
+movl -20(%rbp). %r11d    /*v5 -> %r11d*/
+
+/*Constantes */
+movl %0, %r10d
+movl %1, %r10d
+movl %16, %r10d
+movl %-1, %r10d
+movl %-16, %r10d
+/*Para mover para uma variável local basta dar mov r10d para variável */
+
+
+
